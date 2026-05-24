e7f0998d88cb7a189c7212110c313a57e55a0585
Ezagu <agustincalpedm@gmail.com>
2026-05-24 19:29:57 -0300

    Agregando las materias a profesor

--- src/graphql/resolvers/profesorResolver.ts
+++ src/graphql/resolvers/profesorResolver.ts
@@ -2,6 +2,8 @@ import type { Types } from "mongoose"
 import { Profesor } from "../../database/models/Profesor.js"
 import type { SearchInput } from "../../types/global.js"
 import { normalizarString } from "../../helpers/normalizarString.js"
+import type { IProfesor } from "../../types/profesor.js"
+import { Materia } from "../../database/models/Materia.js"
 
 export const profesorResolver = () =>{
     return {
@@ -11,16 +13,19 @@ export const profesorResolver = () =>{
             },
             profesores: async (_root: undefined, args: SearchInput) => {
                 const {search = "", limit = 10, page = 1} = args
-
+                // Sacamos las tildes del search
                 const searchNormalizado = normalizarString(search)
-                console.log(searchNormalizado)
-
                 // Filtramos con el search y hacemos paginación
                 return await Profesor.find(
                     {nombreNormalizado: {$regex: searchNormalizado, $options: 'i'}}
                 )
                 .skip((page - 1) * limit)
                 .limit(limit)
+            }
+        },
+        Profesor: {
+            materias: async (root: IProfesor) => {
+                return await Materia.find({_id: root.materias})
             }
         }
     }
