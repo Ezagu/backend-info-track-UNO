089ada7311841ac4da2d61c752a5504cb021c61d
Ezagu <agustincalpedm@gmail.com>
2026-06-08 04:18:05 -0300

    modularizando planEstudio

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,21 +1,18 @@
 import { carreraDefs } from "./definitions/carreraDefs.js"
-import { planEstudioDefs } from "./definitions/planEstudioDefs.js"
 import { carreraResolver } from "./resolvers/carreraResolver.js"
 import { resolvers as userResolver, typeDefs as userDefs } from "./user.js"
 import { resolvers as materiaResolver, typeDefs as materiaDefs } from "./materia.js"
 import { resolvers as profesorResolver, typeDefs as profesorDefs } from "./profesor.js"
 import { resolvers as puntuacionResolver, typeDefs as puntuacionDefs } from "./puntuacion.js"
 import { typeDefs as comisionDefs } from "./comision.js"
-
-
-
+import { typeDefs as planEstudioDefs } from "./planEstudio.js"
 
 export const typeDefs = [
   userDefs,
   materiaDefs,
   carreraDefs(),
   comisionDefs,
-  planEstudioDefs(),
+  planEstudioDefs,
   profesorDefs,
   puntuacionDefs
 ]
---
+++ src/graphql/planEstudio.ts
@@ -0,0 +1,9 @@
+export const typeDefs = `
+  type PlanEstudio {
+    id: ID!
+    carreraId: String!
+    materiaId: String!
+    anio: Int!
+    cuatrimestre: Int!
+  }
+`
