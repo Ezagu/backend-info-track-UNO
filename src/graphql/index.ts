906b7ee2a2c3c1968a5d61611e213418b7303ed3
Ezagu <agustincalpedm@gmail.com>
2026-06-08 04:09:10 -0300

    modularizando materia

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,18 +1,17 @@
 import { carreraDefs } from "./definitions/carreraDefs.js"
 import { comisionDefs } from "./definitions/comisionDefs.js"
-import { materiaDefs } from "./definitions/materiaDefs.js"
 import { planEstudioDefs } from "./definitions/planEstudioDefs.js"
 import { profesorDefs } from "./definitions/profesorDefs.js"
 import { puntuacionDefs } from "./definitions/puntuacionDefs.js"
 import { carreraResolver } from "./resolvers/carreraResolver.js"
-import { materiaResolver } from "./resolvers/materiaResolver.js"
 import { profesorResolver } from "./resolvers/profesorResolver.js"
 import { puntuacionResolver } from "./resolvers/puntuacionResolver.js"
 import { resolvers as userResolver, typeDefs as userDefs } from "./user.js"
+import { resolvers as materiaResolver, typeDefs as materiaDefs } from "./materia.js"
 
 export const typeDefs = [
   userDefs,
-  materiaDefs(),
+  materiaDefs,
   carreraDefs(),
   comisionDefs(),
   planEstudioDefs(),
@@ -22,7 +21,7 @@ export const typeDefs = [
 
 export const resolvers = [
   userResolver,
-  materiaResolver(),
+  materiaResolver,
   carreraResolver(),
   profesorResolver(),
   puntuacionResolver()
